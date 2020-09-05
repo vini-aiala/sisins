0212ddaa034f4bb233f05f4ea2e4aa440017ae45
Vinícius <vinicius_aiala@outlook.com>
2020-09-05 02:36:35 -0300

    Ajuste inicial das views

--- resources/views/alunos/index.blade.php
+++ resources/views/alunos/index.blade.php
@@ -14,7 +14,7 @@
 	<body>
 
 		<main>
-			
+
 			<div class="row">
 			    <div class="col s12 m0 l3"></div>
 			    <div class="col s12 m12 l3">
@@ -25,13 +25,13 @@
 				    </div>
 			    </div>
 			    <div class="center col s12 m12 l3">
-			    	<a href="edicao.html" class="center atualizar waves-effect waves-light btn">Atualizar perfil</a>
-	
+			    	<a href="{{ route('alunos-edicao') }}" class="center atualizar waves-effect waves-light btn">Atualizar perfil</a>
+
 					<h5>Seus cursos:</h5>
 					<ul class="collection with-header">
 				        <li class="collection-item">
 							<span class="title"><b>Violão</b></span>
-							<a href="inscricoes.html" class="secondary-content"><i class="material-icons">delete</i></a>
+							<a href="{{ route('alunos-lista') }}" class="secondary-content"><i class="material-icons">delete</i></a>
 							<p>Básico A - Quinta-feira, das 12:00 ás 13:00</p>
 						</li>
 				        <li class="collection-item"><div><i>Curso não selecionado</i><a href="#!" class="secondary-content"><i class="material-icons">add</i></a></div></li>
@@ -44,7 +44,7 @@
 
 		<!--JavaScript para animação e validação de email e senha-->
       <script type="text/javascript" src="../js/materialize.min.js"></script>
-      
+
 
 
 	</body>
