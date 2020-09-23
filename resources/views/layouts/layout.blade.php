4404015fcc64e926ce8e8f1fadff9db15c0439dd
Vinícius <vinicius_aiala@outlook.com>
2020-09-23 01:18:17 -0300

    Adicionado o css da navbar

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,6 +5,7 @@
         <link rel="icon" href="../img/globo.png">
         <meta charset="utf-8">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/header.css') }}" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}" defer></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         @yield('css')
